0125c0381797578e7c697e4f012b2588a87707d6
djangojeng-e <headfat1218@gmail.com>
2019-11-07 16:33:39 +1100

    feat: compare the number and print the message according to the answer input.

--- numguess.py
+++ numguess.py
@@ -4,6 +4,11 @@ answer = randint(0,100+1)
 print(answer) 
 guess = int(input('Hi, guess the num(1~100)'))
 
+if answer == guess:
+    print("You guesses correct Number bro!")
+else:
+    print("You messed up with guess bro! Try again mate!") 
+
 
 
 
