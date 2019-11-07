4fe7e7ff62fe22655fdf425845286e4d798f1b4d
djangojeng-e <headfat1218@gmail.com>
2019-11-07 17:25:20 +1100

    feat: refactoring works been carried out

--- numguess.py
+++ numguess.py
@@ -4,15 +4,17 @@ answer = randint(0,100+1)
 print(answer) 
 
 count = 1
-while count <= 3:
-    guess = int(input('Hi, guess the num(1~100)'))
-    if answer == guess:
-        print("You guesses correct Number bro!")
-        break
-    else:
-        print("You messed up with guess bro! Try again mate!") 
 
-    count = count + 1 
+def compare_guess():
+    while count <= 3:
+        guess = int(input('Hi, guess the num(1~100)'))
+        if answer == guess:
+            print("You guesses correct Number bro!")
+            break
+        else:
+            print("You messed up with guess bro! Try again mate!") 
 
+        count = count + 1 
 
+compare_guess()
 
