ddc44a75ec51d8398b5dd1d3fd723f37ac5ae5ab
djangojeng-e <headfat1218@gmail.com>
2019-11-07 17:08:16 +1100

    feat: repeat 3 times comparing guess, answers added if the guess is correct within 3 times. the repeat stops there.

--- numguess.py
+++ numguess.py
@@ -2,13 +2,17 @@ import random from randint
 
 answer = randint(0,100+1)
 print(answer) 
-guess = int(input('Hi, guess the num(1~100)'))
 
-if answer == guess:
-    print("You guesses correct Number bro!")
-else:
-    print("You messed up with guess bro! Try again mate!") 
+count = 1
+while count <= 3:
+    guess = int(input('Hi, guess the num(1~100)'))
+    if answer == guess:
+        print("You guesses correct Number bro!")
+        break
+    else:
+        print("You messed up with guess bro! Try again mate!") 
 
+    count = count + 1 
 
 
 
